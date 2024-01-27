f540a6532fc7ff48de14a8aa9bcb368bb9f0e06f
AshnaMg <ashnamg1998@gmail.com>
2024-01-27 12:00:34 +0530

    import correction2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Head from './Components/Head';
 import Footer from './Components/Footer'
-import Home from './pages/Home'
-import RestView from './pages/RestView'
+import Home from './Pages/Home'
+import RestView from './Pages/RestView'
 import {Route,Routes} from 'react-router-dom'
 
 function App() {
--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
